dbd76be9ef566ae3038b60ed2b1fa4cee0c7a8fd
CrimsonGhost-55 <wedderburnj2@montclair.edu>
2025-03-24 20:04:30 -0400

    3/24/25 Update
    Making a tower defense game

---
+++ New2DGameXL/Assets/Script/Enemies/Enemy.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : CharacterBase
+{
+    public float speed;
+    public float xSpeed;
+    public float ySpeed;
+    public float xDir;
+    public float yDir;
+    public float reverseTime = 0f;
+    public float reverseInterval = 1f;
+
+    public GameObject thisObject;
+    public GameManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(xSpeed, ySpeed);
+        xDir = Random.Range(-1f, 1f);
+        yDir = Random.Range(-1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayHealth();
+        manager = FindObjectOfType<GameManager>();
+        thisObject.transform.position += new Vector3(xDir, yDir, 0) * speed;
+        reverseTime += Time.deltaTime;
+        if (reverseTime >= reverseInterval)
+        {
+            reverseTime = 0f;
+            xDir = xDir * -1;
+            yDir = yDir * -1;
+        }
+
+        if (health <= 0)
+        {
+            manager.enemyCounter--;
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bombs")
+        {
+            health = health - 5;
+            Destroy(other.gameObject);
+        }
+    }
+}
